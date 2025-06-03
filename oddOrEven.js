4c97ba46afe48e1911e6fee900d72688fe21d3bb
S-ANANTHI <72901182+S-ANANTHI@users.noreply.github.com>
2025-06-03 15:35:54 +0530

    Update oddOrEven.js

--- oddOrEven.js
+++ oddOrEven.js
@@ -2,3 +2,11 @@ function oddEven(num){
     return (num%2 == 0)? "even" : "odd";
 }
 console.log("Function " +oddEven(3));
+
+
+//--------------
+function odd(n){
+    let a=n.filter(acc=>acc%2==0);
+    return a;
+}
+console.log(odd([2,3,4,5]));
