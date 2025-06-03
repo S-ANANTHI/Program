90bbd310ebda084dcbfb9c6ea6a8f3443732eda2
S-ANANTHI <72901182+S-ANANTHI@users.noreply.github.com>
2025-06-03 15:37:50 +0530

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -7,3 +7,22 @@ function p(name){
 }
 console.log(p("malayalam"));
 console.log(p("MAlayalam"));
+
+//---------------------
+let a=(word)=>{
+    let c="";
+    for (let i=word.length-1;i>=0;i--){
+        c+=word[i];
+    }
+    if (word==c){
+        return "P";
+    }else return "not p";
+}
+console.log(a("malayalam"));
+//-----------------
+
+let z= (words)=>{
+    let y = words.split("").reverse().join("");
+    return (words==y) ?"p":"not p";
+}
+console.log(z("malayalam"));
